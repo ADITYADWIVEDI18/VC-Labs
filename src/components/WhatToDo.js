8cb9a9869f3e3ab3baff63c90b27bb6621453c64
Aditya Dwivedi <adityaadwivedi18@gmail.com>
2024-07-06 18:04:53 +0530

    Fix WhatToDo

--- src/components/WhatToDo.js
+++ src/components/WhatToDo.js
@@ -1,29 +1,27 @@
-import React from 'react'
+import React from 'react';
 import ToDoCard from '../cards/ToDoCard';
 import { info } from '../utils/ToDoInfo';
 import DoThis from './DoThis';
+
 const WhatToDo = () => {
-   
-
     return (
-        <div className='bg-white w-full min-h-full pb-8 flex flex-col justify-center items-center'>
-            <div className=''>   
-            <p className='  font-anton text-[4vmax] text-center text-black pl-16 pt-10'>What we do</p>
-            </div>
-           
-            <div className=' flex flex-wrap justify-center items-center'>
-            {info.map((item, index) => (
-                <ToDoCard
-                    key={index}
-                    heading={item.heading}
-                    subheading={item.subheading}
-                    image={item.image}
-                />
-            ))}
+        <div className='bg-white w-full min-h-full pb-8 flex flex-col justify-center items-center p-8 gap-8'>
+            <div className='text-center'>
+                <p className='font-anton text-[3em] sm:text-[4em] md:text-6xl lg:text-black mt-10'>What we do</p>
             </div>
 
+            <div className='flex flex-wrap justify-center items-center'>
+                {info.map((item, index) => (
+                    <ToDoCard
+                        key={index}
+                        heading={item.heading}
+                        subheading={item.subheading}
+                        image={item.image}
+                    />
+                ))}
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default WhatToDo;
